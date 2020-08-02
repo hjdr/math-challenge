16381ba6c6c6a9041d2033201234a46780e6e2e8
hjdr <harry.jdr@gmail.com>
2020-08-02 23:11:37 +0100

    refactored and added subtraction expressions

--- src/calculator.spec.ts
+++ src/calculator.spec.ts
@@ -10,43 +10,67 @@ describe('calculator()', () => {
   });
 
   describe('WHEN given an expression with one + operator', () => {
-    it('SHOULD return the result of the addition', () => {
+    it('SHOULD return the result of the expression', () => {
       expect(calculator('3+3')).toBe(6)
     });
   });
 
+  describe('WHEN given an expression with > one digit integers', () => {
+    it('SHOULD return the result of the expression', () => {
+      expect(calculator('373+2072')).toBe(2445)
+    });
+  });
+
   describe('WHEN given an expression with two + operators', () => {
-    it('SHOULD return the result of the addition', () => {
+    it('SHOULD return the result of the expression', () => {
       expect(calculator('3+3+3')).toBe(9)
     });
   });
 
   describe('WHEN given an expression with multiple + operators', () => {
-    it('SHOULD return the result of the addition', () => {
+    it('SHOULD return the result of the expression', () => {
       expect(calculator('3+3+3+3+3+3+3')).toBe(21)
     });
   });
 
+  describe('WHEN given an expression with one - operator', () => {
+    it('SHOULD return the result of the expression', () => {
+      expect(calculator('9-4')).toBe(5)
+    });
+  });
+
+  describe('WHEN given an expression with multiple - operators', () => {
+    it('SHOULD return the result of the expression', () => {
+      expect(calculator('9-2-2-2')).toBe(3)
+    });
+  });
+
+  describe('WHEN given an expression with - and + operators', () => {
+    it('SHOULD return the result of the expression following left associativity', () => {
+      expect(calculator('9-2-3+2+2')).toBe(8)
+    });
+  });
+
   describe('WHEN given an expression with multiple * operators', () => {
-    it('SHOULD return the result of the addition', () => {
+    it('SHOULD return the result of the expression', () => {
       expect(calculator('8*8*8*8')).toBe(4096)
     });
   });
 
   describe('WHEN given an expression with + and * operators', () => {
-    it('SHOULD return the result of the addition', () => {
+    it('SHOULD return the result of the expression', () => {
       expect(calculator('8+8*8')).toBe(72)
     });
   });
 
   describe('WHEN given an expression with * and + operators', () => {
-    it('SHOULD return the result of the addition', () => {
+    it('SHOULD return the result of the expression', () => {
       expect(calculator('8+8*8')).toBe(72)
     });
   });
 
   describe('WHEN given an expression multiple * and + operators', () => {
-    it('SHOULD return the result of the addition', () => {
+    it('SHOULD return the result of the expression', () => {
       expect(calculator('8+8*8+8*8+8+8')).toBe(152)
     });
   });
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,16 +1,17 @@
 
 export default function calculator(expression: string) {
-  let tokens: Array<string> = expression.split('')
+  let tokens: Array<string> = expression.split(/([-+*/()])/)
   let loopBreaker = 0
 
   enum TokenType {
+    Minus = '-',
     Multiply = '*',
     Plus = '+',
   }
 
   while (tokens.length > 1 && loopBreaker < 30) {
     console.log('start', tokens)
-    addition();
+    scanTokens()
     loopBreaker += 1;
     console.log('finish', tokens)
   }
@@ -23,25 +24,52 @@ export default function calculator(expression: string) {
     executeExpression(TokenType.Plus);
   }
 
-  function multiply(){
-    executeExpression(TokenType.Multiply);
+  function execute(previousToken: number, nextToken: number, tokenType: string) {
+    if (tokenType === TokenType.Minus) return previousToken - nextToken;
+    if (tokenType === TokenType.Plus) return previousToken + nextToken;
+    if (tokenType === TokenType.Multiply) return previousToken * nextToken;
   }
 
   function executeExpression(tokenType: string) {
     for (const [index, token] of tokens.entries()) {
       const previousToken = Number(tokens[index - 1]);
       const nextToken = Number(tokens[index + 1]);
-      if (token === TokenType.Plus && tokenType === TokenType.Plus) {
-        const sum = previousToken + nextToken
-        tokens.splice(index -1, 3, String(sum))
-        break;
-      }
-      if (token === TokenType.Multiply && tokenType === TokenType.Multiply) {
-        const sum = previousToken * nextToken
+
+      if (token === tokenType) {
+        const sum = execute(previousToken, nextToken, tokenType);
         tokens.splice(index -1, 3, String(sum))
         break;
       }
     }
+  }
+
+  function multiply(){
+    executeExpression(TokenType.Multiply);
+  }
+
+  function scanTokens(){
+    for (const token of tokens) {
+      if (token === TokenType.Minus) {
+        subtraction();
+        break;
+      }
+      if (token === TokenType.Plus) {
+        addition();
+        break;
+      }
+      if (token === TokenType.Multiply) {
+        multiply();
+        break;
+      }
+    }
+  }
+
+  function subtraction() {
+    if (tokens.includes(TokenType.Multiply)) {
+      multiply();
+      return;
+    }
+    executeExpression(TokenType.Minus);
   }
 
   return Number(tokens[0]);
